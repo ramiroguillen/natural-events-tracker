cbb4966f59ca5b722cb4f0cfde70b69209df04d1
ramiroguillen <ramirojguillen@gmail.com>
2022-04-19 13:21:16 -0300

    responsive

--- src/components/LocationInfoBox.jsx
+++ src/components/LocationInfoBox.jsx
@@ -1,7 +1,7 @@
 const LocationInfoBox = ({ info }) => {
     return (
         <div className="location-info">
-            <h2>Event Location Info</h2>
+            <h2>Event Info</h2>
             <ul>
                 <li>ID: <strong>{info.id}</strong></li>
                 <li>TITLE: <strong>{info.title}</strong></li>
